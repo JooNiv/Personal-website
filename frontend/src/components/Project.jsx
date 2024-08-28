21fa4a83c026497331a4e9158f02ef0730a7d06b
Joonas Nivala <joonas.nivala@gmail.com>
2024-08-28 20:46:29 +0300

    edit project cards

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -1,7 +1,7 @@
 const Project = ({title, description, link, image_path}) => {
     return (
         <div className="card card-compact bg-base-100 w-80 shadow-xl m-1 mb-10">
-          <figure>
+          <figure className="block border-[14px] rounded-[23px] border-base-100">
             <img
               src={image_path}
               alt="Circuit Knitting on FiQCI" />
